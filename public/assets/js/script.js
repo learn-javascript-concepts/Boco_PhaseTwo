65a1202db91de7338374635f5efa2bc3fc1ae202
learn-javascript-concepts <learn-javascript-concepts@gmail.com>
2018-03-10 13:07:53 +0530

    Changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,3 +6,13 @@ $(".pageScollTop").click(function() {
 	$("html, body").animate({ scrollTop: 0 }, "slow");
 	return false;
 });
+
+$('ul.tabs li').click(function() {
+	var tab_id = $(this).find('a').attr('data-tab');
+
+	$('ul.tabs li a').removeClass('active');
+	$('.tab-content').removeClass('current');
+
+	$(this).find('a').addClass('active');
+	$("#"+tab_id).addClass('current');
+});
